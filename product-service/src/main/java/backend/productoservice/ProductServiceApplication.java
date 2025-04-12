96eb7f88c445a089ff0e55f92005b94a2224ac17
Victor Orbegozo <victor.orbegozo@codingcrew.la>
2025-04-11 22:36:40 -0500

    [product] se modifico test de controladores

--- product-service/src/main/java/backend/productoservice/ProductServiceApplication.java
+++ product-service/src/main/java/backend/productoservice/ProductServiceApplication.java
@@ -2,8 +2,10 @@ package backend.productoservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class ProductServiceApplication {
 
     public static void main(String[] args) {
