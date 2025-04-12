fb56ec6a7a977a82cc6bb092bd52e0b1cc8368b1
Victor Orbegozo <victor.orbegozo@codingcrew.la>
2025-04-11 22:06:07 -0500

    [product] se modifico pruebas unitarias

--- product-service/src/test/java/backend/productoservice/services/impl/ProductoServiceImplTest.java
+++ product-service/src/test/java/backend/productoservice/services/impl/ProductoServiceImplTest.java
@@ -5,13 +5,21 @@ import backend.productoservice.models.dto.request.ProductoDtoRequest;
 import backend.productoservice.models.dto.response.ProductoDtoResponse;
 import backend.productoservice.models.entities.Producto;
 import backend.productoservice.repositories.ProductoRepository;
+import backend.productoservice.util.Paginado;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -121,39 +129,50 @@ class ProductoServiceImplTest {
         assertThrows(ProductoException.class, () -> service.updateStock(1, -10, "SALIDA"));
     }
 
-//    @Test
-//    void testBuscarPorNombre_ProductoNoExiste_RetornaNull() {
-//        // Arrange
-//        when(repository.findByNombre("Producto 1")).thenReturn(Optional.empty());
-//
-//        // Act
-//        ProductoDtoResponse producto = service.getByName("Producto 1");
-//
-//        // Assert
-//        assertThat(producto).isNull();
-//    }
+    @Test
+    void testBuscarPorNombre_ProductoNoExiste_RetornaNull() {
+        Paginado paginado = new Paginado(1, 10, "id");
 
-//    @Test
-//    void testBuscarPorNombre_ProductoExiste_RetornaProducto() {
-//        // Arrange
-//        Producto producto = Producto.builder()
-//                .id(1)
-//                .nombre("Producto 1")
-//                .descripcion("Descripcion 1")
-//                .precio(100.0)
-//                .build();
-//
-//        // Act
-//        when(repository.findByNombre("Producto 1")).thenReturn(Optional.of(producto));
-//        ProductoDtoResponse productoDtoResponse = service.getByName("Producto 1");
-//
-//        // Assert
-//        assertThat(productoDtoResponse).isNotNull();
-//        assertThat(productoDtoResponse.id()).isEqualTo(1);
-//        assertThat(productoDtoResponse.nombre()).isEqualTo("Producto 1");
-//        assertThat(productoDtoResponse.descripcion()).isEqualTo("Descripcion 1");
-//        assertThat(productoDtoResponse.precio()).isEqualTo(100.0);
-//    }
+        // Arrange
+        when(repository.findAllByNombreIgnoreCaseContaining(any(String.class), any(Pageable.class))).thenReturn(Page.empty());
+
+        // Act
+        Page<ProductoDtoResponse> producto = service.listByname("Producto 1", paginado);
+
+        // Assert
+        assertThat(producto).isNull();
+    }
+
+    @Test
+    void testBuscarPorNombre_ProductoExiste_RetornaProducto() {
+        // Arrange
+        Producto producto = Producto.builder()
+                .id(1)
+                .nombre("Producto 1")
+                .descripcion("Descripcion 1")
+                .precio(100.0)
+                .estado(true)
+                .fechaCreacion(LocalDate.of(2024, 1, 1))
+                .stock(20)
+                .build();
+
+        List<Producto> listaProductos = Collections.singletonList(producto);
+
+        Pageable pageable = PageRequest.of(1 - 1, 10);
+
+        Paginado paginado = new Paginado(1, 10, "id");
+
+        Page<Producto> page = new PageImpl<>(listaProductos, pageable, listaProductos.size());
+
+        // Act
+        when(repository.findAllByNombreIgnoreCaseContaining(any(String.class), any(Pageable.class))).thenReturn(page);
+        Page<ProductoDtoResponse> productoDtoResponse = service.listByname("Producto 1", paginado);
+
+        // Assert
+        assertThat(productoDtoResponse).isNotNull();
+        assertThat(productoDtoResponse.getTotalElements()).isEqualTo(1);
+
+    }
 
     @Test
     void testBuscarPorId_ProductoExiste_RetornaProducto() {
@@ -189,51 +208,51 @@ class ProductoServiceImplTest {
         assertThat(producto).isNull();
     }
 
-//    @Test
-//    void testListar_SinProductosEnBD_RetornaListaVacia() {
-//        // Arrange
-//
-//        // Act
-//        when(repository.findAll()).thenReturn(List.of());
-//        List<ProductoDtoResponse> productos = service.listAll();
-//
-//        // Assert
-//        assertThat(productos).isNotNull();
-//        assertThat(productos).isEmpty();
-//    }
+    @Test
+    void testListar_SinProductosEnBD_RetornaListaVacia() {
+        // Arrange
+        when(repository.findAll(any(Pageable.class))).thenReturn(Page.empty());
 
-//    @Test
-//    void testListar_ProductosEnBD_RetornaListaProductos() {
-//        // Arrange
-//        Producto producto1 = Producto.builder()
-//                .id(1)
-//                .nombre("Producto 1")
-//                .descripcion("Descripcion 1")
-//                .precio(100.0)
-//                .build();
-//        Producto producto2 = Producto.builder()
-//                .id(2)
-//                .nombre("Producto 2")
-//                .descripcion("Descripcion 2")
-//                .precio(200.0)
-//                .build();
-//
-//        // Act
-//        when(repository.findAll()).thenReturn(List.of(producto1, producto2));
-//        List<ProductoDtoResponse> productos = service.listAll();
-//
-//        // Assert
-//        assertThat(productos).isNotNull();
-//        assertThat(productos).hasSize(2);
-//        assertThat(productos.get(0).id()).isEqualTo(1);
-//        assertThat(productos.get(0).nombre()).isEqualTo("Producto 1");
-//        assertThat(productos.get(0).descripcion()).isEqualTo("Descripcion 1");
-//        assertThat(productos.get(0).precio()).isEqualTo(100.0);
-//        assertThat(productos.get(1).id()).isEqualTo(2);
-//        assertThat(productos.get(1).nombre()).isEqualTo("Producto 2");
-//        assertThat(productos.get(1).descripcion()).isEqualTo("Descripcion 2");
-//        assertThat(productos.get(1).precio()).isEqualTo(200.0);
-//    }
+        // Act
+        Page<ProductoDtoResponse> productos = service.listAll(1, 10, "id");
+
+        // Assert
+        assertThat(productos.getTotalElements()).isEqualTo(0);
+    }
+
+    @Test
+    void testListar_ProductosEnBD_RetornaListaProductos() {
+        // Arrange
+        Producto producto1 = Producto.builder()
+                .id(1)
+                .nombre("Producto 1")
+                .descripcion("Descripcion 1")
+                .precio(100.0)
+                .estado(true)
+                .fechaCreacion(LocalDate.of(2024, 1, 1))
+                .stock(20)
+                .build();
+        Producto producto2 = Producto.builder()
+                .id(2)
+                .nombre("Producto 2")
+                .descripcion("Descripcion 2")
+                .precio(200.0)
+                .estado(true)
+                .fechaCreacion(LocalDate.of(2024, 1, 1))
+                .stock(40)
+                .build();
+
+        List<Producto> listaProductos = Arrays.asList(producto1, producto2);
+        Pageable pageable = PageRequest.of(1 - 1, 10);
+
+        // Act
+        when(repository.findAll(any(Pageable.class))).thenReturn(new PageImpl<>(listaProductos, pageable, listaProductos.size()));
+        Page<ProductoDtoResponse> productos = service.listAll(1, 10, "id");
+
+        // Assert
+        assertThat(productos).isNotNull();
+        assertThat(productos).hasSize(2);
+    }
 
     // Test methods here
     @Test
