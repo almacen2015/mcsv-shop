0d6c4e4617eaf974eaaad328a67eeeb59d1c6269
Victor Orbegozo <victor.orbegozo@codingcrew.la>
2025-04-30 16:58:50 -0500

    se cambiaron nombres en uris de gateway dev y test

--- sale-service/src/main/java/backend/saleservice/client/MovementClient.java
+++ sale-service/src/main/java/backend/saleservice/client/MovementClient.java
@@ -5,7 +5,7 @@ import backend.saleservice.models.dtos.response.MovementResponseDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 
-@FeignClient(name = "mcsv-movimientos", url = "${movement.service.url}")
+@FeignClient(name = "inventory-service", url = "${movement.service.url}")
 public interface MovementClient {
     @PostMapping
     MovementResponseDto createMovimientoDto(MovementRequestDto movimientoDtoRequest);
